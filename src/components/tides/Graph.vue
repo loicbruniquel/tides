731433b7fc9d5d69e59f0ac75440453075c5f623
Loïc Bruniquel <loicbr@gmail.com>
2020-02-01 11:18:17 +0100

    smooth tide graph with extremes points

--- src/components/tides/Graph.vue
+++ src/components/tides/Graph.vue
@@ -2,41 +2,89 @@
   <div class="graph-container">
 
     <svg
+      ref="svgGraph"
       height="100%"
       width="100%"
       viewBox="0 0 100 100"
       preserveAspectRatio="none"
       class="graph">
       <path :d="svgPath" fill="transparent" stroke="red" stroke-width="3" vector-effect="non-scaling-stroke" />
+      <ellipse
+        v-for="point in extremeData"
+        :key="`${point.x}-${[point.y]}`"
+        :cx="getPercentValue(point.x, minX, maxX)"
+        :cy="getPercentValue(point.y, minY, maxY)"
+        :rx="dotRadius"
+        :ry="dotHeight"
+        stroke-width="0" fill="blue" />
     </svg>
-    <pre>{{minHeight}} || {{maxHeight}}</pre>
-    <pre>{{heightData}}</pre>
   </div>
 </template>
 
 <script>
-import { svgPath } from 'src/utils/plot.js'
+import { svgPath, getPercentValue } from 'src/utils/plot'
 
 export default {
   name: 'TideGraph',
   props: {
     tides: { type: Object, required: true }
   },
+  data () {
+    return {
+      svgDimensions: { x: 0, y: 0 }
+    }
+  },
   computed: {
     svgPath () {
-      return svgPath(this.heightData, this.minHeight, this.maxHeight)
+      return svgPath(this.heightData, this.minX, this.maxX, this.minY, this.maxY)
     },
     heightData () {
       return this.tides.heights.map(height => {
         return { x: height.dt, y: height.height }
       })
     },
-    minHeight () {
+    extremeData () {
+      return this.tides.extremes.map(extreme => {
+        return { x: extreme.dt, y: extreme.height }
+      })
+    },
+    minX () {
+      return this.heightData[0].x
+    },
+    maxX () {
+      return this.heightData[this.heightData.length - 1].x
+    },
+    minY () {
+      return this.tides.datums.find(datum => datum.name === 'HAT').height
+    },
+    maxY () {
       return this.tides.datums.find(datum => datum.name === 'LAT').height
     },
-    maxHeight () {
-      return this.tides.datums.find(datum => datum.name === 'HAT').height
+    dotRadius () {
+      return 100 / this.svgDimensions.w * 10
+    },
+    dotHeight () {
+      return this.dotRadius * this.svgAspectRatio
+    },
+    svgAspectRatio () {
+      return this.svgDimensions.w / this.svgDimensions.h
     }
+  },
+  methods: {
+    getPercentValue,
+    handleResize (event) {
+      this.svgDimensions = { w: this.$refs.svgGraph.clientWidth, h: this.$refs.svgGraph.clientHeight }
+      console.log(this.aspectRatio)
+    }
+  },
+  mounted () {
+    this.handleResize()
+  },
+  created: function () {
+    window.addEventListener('resize', this.handleResize)
+  },
+  beforeDestroy: function () {
+    window.removeEventListener('resize', this.handleResize)
   }
 }
 </script>
--- src/utils/plot.js
+++ src/utils/plot.js
@@ -1,38 +1,99 @@
-let getPercentCoord = function (dt, startValue, endValue) {
-  let span = endValue - startValue
-  let offset = dt - startValue
-  return (offset / span * 100).toFixed(2)
+const SMOOTH_RATIO = 0.2
+
+/**
+ * Returns a percentage of the value in reference to the min and max values
+ * @param {float} value
+ * @param {float} minValue
+ * @param {float} maxValue
+ */
+export let getPercentValue = function (value, minValue, maxValue) {
+  let span = maxValue - minValue
+  let offset = value - minValue
+  return parseFloat(((offset / span) * 100).toFixed(2))
 }
 
-export let svgPath = function (heights, maxHeight, minHeight) {
-  let startValueX = heights[0].x
-  let endValueX = heights[heights.length - 1].x
+/**
+ * Returns the properties (length and angle in radians) of the line between the two points
+ * @param {object} pointA
+ * @param {object} pointB
+ */
+let line = function (pointA, pointB) {
+  const lengthX = pointB.x - pointA.x
+  const lengthY = pointB.y - pointA.y
+  return {
+    length: Math.sqrt(Math.pow(lengthX, 2) + Math.pow(lengthY, 2)),
+    angle: Math.atan2(lengthY, lengthX)
+  }
+}
 
-  let startY = getPercentCoord(heights[0].y, minHeight, maxHeight)
+/**
+ * Returns a control point coordinates
+ * @param {object} current The reference point (source of the control point)
+ * @param {object} previous Point before the reference point
+ * @param {object} next Point after the reference point
+ * @param {bool} reverse True if it's the end control point
+ */
+let controlPoint = function (current, previous, next, reverse) {
+  // if no previous or next, use current
+  previous = previous || current
+  next = next || current
 
-  let index = 0
+  // getting info about line opposite to point (length and angle)
+  const referenceLine = line(previous, next)
 
-  let coords = heights.map(height => {
-    let x = getPercentCoord(height.x, startValueX, endValueX)
-    let y = getPercentCoord(height.y, minHeight, maxHeight)
-    let str = null
+  // if end control point, reverse angle by adding PI
+  const angle = referenceLine.angle + (reverse ? Math.PI : 0)
+
+  // ajusting length of control point with smooth ratio
+  const length = referenceLine.length * SMOOTH_RATIO
+
+  // calculating x,y from angle and lenth, relative to current point
+  const x = current.x + Math.cos(angle) * length
+  const y = current.y + Math.sin(angle) * length
+
+  return { x, y }
+}
+
+/**
+ * Returns a bezier command string for the point
+ * @param {object} point
+ * @param {number} index
+ * @param {array} allPoints
+ */
+let bezierCommand = function (point, index, allPoints) {
+  let oppositePoint = allPoints[index - 2]
+  let previousPoint = allPoints[index - 1]
+  let nextPoint = allPoints[index + 1]
+
+  // start control point, using line between 2 previous point as reference
+  const startCP = controlPoint(previousPoint, oppositePoint, point)
+
+  // end control point, using line between previous and next point as reference
+  const endCP = controlPoint(point, previousPoint, nextPoint, true)
+
+  return `C ${startCP.x},${startCP.y} ${endCP.x},${endCP.y} ${point.x},${point.y}`
+}
+
+/**
+ * Returns a smoothed svg path string
+ * @param {object} points Each point is an object with x and y coordinate
+ * @param {float} minY
+ * @param {float} maxY
+ * @returns {string}
+ */
+export let svgPath = function (points, minX, maxX, minY, maxY) {
+  return points.map(point => {
+    return {
+      x: getPercentValue(point.x, minX, maxX),
+      y: getPercentValue(point.y, minY, maxY)
+    }
+  }).reduce((pathString, point, index, allPoints) => {
     if (index === 0) {
-      str = `L${x} ${y}`
-    } else if (index === 1) {
-      str = `Q${x} ${y}`
-    } else if (index === 2) {
-      str = `${x} ${y}`
+      // first point is move command
+      return `M ${point.x},${point.y}`
     } else {
-      throw new Error('Index must be less than 3')
+      // adding bezier command for each point
+      return `${pathString} ${bezierCommand(point, index, allPoints)}`
     }
-    index++
-    if (index >= 3) {
-      index = 0
-    }
-    return str
-  })
-  let pathString = coords.join(' ')
-  console.log(pathString)
-  return `M0 ${startY} ${pathString}`
-  // return 'M0 100 Q 50 0 100 100'
+  }, '')
 }
